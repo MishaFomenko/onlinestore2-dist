983c7b5f85a93b177e5c862025622e33ef19c41d
Mykhailo Fomenko <misha.fomenko00@gmail.com>
2023-11-03 23:41:45 -0400

    changing the homepage title

--- frontend/app/components/shop.jsx
+++ frontend/app/components/shop.jsx
@@ -30,7 +30,7 @@ export default function Shop() {
                     </div>
                     :
                     <>
-                        <div className='h-16 bg-blue-400 flex items-center p-6'>Best Sellers</div>
+                        <div className='h-16 bg-blue-400 flex items-center p-6'>Recommended products</div>
                         <div className='flex flex-wrap'>
                             {!isLoading && bs.map((item) => <ProductCard key={item.asin} item={item} />)}
                         </div>
